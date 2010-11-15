81d0c86c3decd212a7e1349d747ce834d3ac315b
Mike Blyth <mike.blyth@sim.org>
2010-11-15 10:26:43 +0100

    Along the way of making JS to compose full name in member form.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,9 +7,26 @@
           source: "autocomplete/country.js"
           });
       });
+// Enable the default-disabled :name field in the member form
     $('a.allow_edit').live('click', function() {
       $(this).prev().removeAttr("disabled")
       .focus()
       });
+// Build full name from components
+    $('.name-input').live('click', function(){
+      $(this).val('name clicked')
+    });
+    $('.first_name-input').live('click', function(){
+      var as_form = $(this).closest("form");
+      var last_name = $('.last_name-input',as_form).val()
+      var first_name = $('.first_name-input',as_form).val()
+      var initial = $('.middle_name-input',as_form).val()[0] + '.'
+      var short_name = $('.short_name-input',as_form).val()
+      var name = last_name + ', ' + first_name + ' ' + initial
+      if (short_name) { 
+        name += ' (' + short_name + ')'
+      }
+      $('.name-input',as_form).val(name);
+    });
   });
 
