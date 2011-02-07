e7339d85a802606878d316c98cd8a3b53bd5c05f
Mike Blyth <mike.blyth@sim.org>
2011-02-07 13:49:23 +0100

    in progress

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,8 @@ config.action_controller.perform_caching = false
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
   config.serve_static_assets = true
-
+  ACTIVE_SCAFFOLD_INSTALL_ASSETS = false 
+  
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
