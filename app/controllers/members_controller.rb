5781f208c3097e12d5bb5f6f73805fbaf5b5ed0e
Mike Blyth <mike.blyth@sim.org>
2011-04-27 09:55:53 +0100

    in progress

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -10,11 +10,11 @@ class MembersController < ApplicationController
 
   active_scaffold :member do |config|
 
-  #  config.actions << :config_list
+    config.actions << :config_list
 
     config.label = "Members"
     list.columns = [:name, :spouse, 
-          :child, :residence_location, :work_location, :ministry, :health_data, :travels, :field_terms, :status, :contacts]
+          :child, :residence_location, :work_location, :ministry, :travels, :field_terms, :status, :contacts]
     config.columns[:name].sort_by :sql
     config.list.sorting = {:name => 'ASC'}
     show.columns = create.columns = update.columns = [:name, :name_override,
