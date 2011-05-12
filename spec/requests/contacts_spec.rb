9d633624c679917605bb49c424a232c6e4e0e536
Mike Blyth <mike.blyth@sim.org>
2011-05-12 23:37:24 +0100

    Contact privacy changed from phone_public etc to phone_private etc. Integration test added for contact form. Cucumber updated. Some of these changes were in previous 'in progress' commits. All tests pass.

---
+++ spec/requests/contacts_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+  include ApplicationHelper
+
+describe 'Contacts' do
+
+    before(:each) do
+     # require 'cleaner'
+      integration_test_sign_in(:admin=>true)
+      @contact_type = Factory(:contact_type)
+      @form_data = {'address'=>'MyAddress', 'blog' => 'MyBlog', 'contact_name'=>'my Contact name',
+                    'email_1' => 'me@example.com', 'email_2' => 'me2@example.com',
+                    'phone_1' => '0808-888-8888', 'phone_1' => '0808-888-8889', 
+                    'skype' => 'MySkype', 'facebook' => 'MyFacebook', 'photos' => 'MyPhotos',
+                    'other_website'=> 'My Other Website'
+                    }
+    end  
+
+  
+    it "should add a contact record with all info for an existing member" do
+      lambda do
+        member = Factory(:family).head
+        visit members_path
+        click_link "as_members-edit-#{member.id}-link"
+##save_and_open_page
+        within(:xpath, "//li[@id='as_members-#{member.id}-contacts-subform']") do
+          @form_data.each {|key, value| fill_in key.humanize, :with=>value}
+          check 'Phone private'
+          check 'Email private'
+          select @contact_type.description
+        end
+        click_link_or_button('Update')
+      end.should change(Contact, :count).by(1)
+      c = Contact.last
+      @form_data.each {|key, value| c.send(key).should == value}
+      c.phone_private.should be_true
+      c.email_private.should be_true
+      c.skype_private.should be_false
+    end      
+
+end
