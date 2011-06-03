de07d017f18fdf69d5bf3d95d48c19088281b246
Mike Blyth <mike.blyth@sim.org>
2011-06-03 13:59:04 +0100

    in progress

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SIM::Application.config.secret_token = '74f0ccee1be860d36d34c0102b0f1c69bd680eb6d532d2a0472d8e0bafa157be437206925d650c86802dfbae90c43f65a3d805aaa7aed7ab9a5a359b13d4c8ca'
+SIM::Application.config.secret_token = '74f0ccee1b2b0f1c69bd680eb6d4532d2a0472d8e0ba3fa157be437206925d650c86802dfbae90c43f65a3d805aaa7aed7ab9a5a359b13d4c8ca'
