2d1e60d80e7d0a00790d539459c5eb3b3a1930ef
Mike Blyth <mike.blyth@sim.org>
2012-04-18 15:01:45 +0100

    in progress

---
+++ db/migrate/20120418133522_create_travel_member_join_table.rb
@@ -0,0 +1,14 @@
+class CreateTravelMemberJoinTable < ActiveRecord::Migration
+  def self.up
+    create_table :members_travels, :id => false do |t|
+      t.integer :member_id
+      t.integer :travel_id
+    end
+
+
+  end
+
+  def self.down
+    drop_table :members_travels
+  end
+end
