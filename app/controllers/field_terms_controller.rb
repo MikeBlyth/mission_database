cd74bbcc253e0ec0bc9a8b8d903624a4b3e55c1b
Mike Blyth <mike.blyth@sim.org>
2012-05-18 11:09:08 +0100

    Added end_ and start_estimated columns.

--- app/controllers/field_terms_controller.rb
+++ app/controllers/field_terms_controller.rb
@@ -4,11 +4,15 @@ class FieldTermsController < ApplicationController
   include AuthenticationHelper
   
   active_scaffold :field_term do |config|
-    config.columns = [:start_date, :end_date, :ministry, :primary_work_location, :employment_status]
+    config.columns = [:start_date, :start_estimated, :end_date, :end_estimated, :ministry, :primary_work_location, :employment_status]
  #  config.subform.layout = :vertical
 #    config.columns[:start_date].form_ui = :select 
     config.columns[:start_date].inplace_edit = true
     config.columns[:end_date].inplace_edit = true
+    config.columns[:start_estimated].inplace_edit = true
+    config.columns[:start_estimated].label = "Est?"
+    config.columns[:end_estimated].inplace_edit = true
+    config.columns[:end_estimated].label = "Est?"
     config.columns[:ministry].form_ui = :select 
     config.columns[:ministry].inplace_edit = true
 #    config.columns[:primary_work_location].form_ui = :select 
