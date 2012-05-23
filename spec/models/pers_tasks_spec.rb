8c5572f42dbf962ac2e9113220a79d0ad39347e7
Mike Blyth <mike.blyth@sim.org>
2012-05-23 15:06:33 +0100

    in progress

--- spec/models/pers_tasks_spec.rb
+++ spec/models/pers_tasks_spec.rb
@@ -35,40 +35,5 @@ describe PersTask do
     result_tasks[0].finished.should be_false
     result_tasks[1].finished.should be_false
   end
-#  it 'task_ids returns list of task ids for given type' do
-#    result =  @all_tasks.tasks_with_type(:pipeline)
-#puts "**** result=#{result}"
-#    result.should include @t1
-#    result.should include @t2
-#    result.should_not include @x1
-#    result.should_not include @x2
-#  end    
-#  
-#  it 'tasks_hash returns an ordered array of all tasks by type' do
-#  # result[0] should be {:type=>:pipeline, :tasks=>[@t1, @t2]}
-#  # result[1] should be {:type=>:orientation, :tasks=>[@x1, @x2]}
-#  # result[2] should be {:type=>:start_of_term, :tasks=>[]}
-#    result =  @all_tasks.tasks_hash
-## puts "**** result=#{result}"
-#    result[0][:type].should == :pipeline
-#    result_tasks = result[0][:tasks]
-#    result_tasks.should include @t1
-#    result_tasks.should include @t2
-#    result_tasks.should_not include @x1
-#    result_tasks.should_not include @x2
-#    result_tasks = result[1][:tasks]
-#    result_tasks.should include @x1
-#    result_tasks.should include @x2
-#    result_tasks.should_not include @t1
-#    result_tasks.should_not include @t2
-#    result[2][:tasks].should be_empty
-#  end
-#      
-#  it 'generates string w ids of finished tasks' do
-#    @t1.finished = true
-#    @x1.finished = true
-#    result =  @all_tasks.finished_tasks_string
-#    result.should == "#{@t1.id},#{@x1.id}"
-#  end
 
 end
