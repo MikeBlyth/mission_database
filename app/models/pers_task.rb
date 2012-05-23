9d4c59c7927fe1a9d5035b28fc984c71c59406af
Mike Blyth <mike.blyth@sim.org>
2012-05-23 11:23:19 +0100

    In progress, all tests pass

--- app/models/pers_task.rb
+++ app/models/pers_task.rb
@@ -19,6 +19,8 @@ class PersTask < ActiveRecord::Base
   include ModelHelper
   validates_presence_of :task
 
+  attr_accessor :finished
+
   def to_s
     self.task
   end
@@ -29,13 +31,24 @@ class PersTask < ActiveRecord::Base
 
   # For a type (like :pipeline or :orientation), return the array of task ids that
   # are defined for that type
-  def self.task_ids(type)
-    self.all.map {|t| t.id if t.send(type)}.compact
-  end
+#  def self.task_ids(type)
+#    self.all.map {|task| task.id if task.send(type)}.compact
+#  end
   
-  def self.tasks_hash
-    self.task_types.map {|typ| {:type=>typ, :ids=>self.task_ids(typ)} }
-  end
+#  def self.tasks_with_type(type)
+#    self.all.map {|task| task if task.send(type)}.compact
+#  end
+  def self.tasks_with_type(task_array,type)
+    task_array.clone.keep_if {|task| task.send(type)}
+  end    
+  
+#  def self.tasks_hash
+#    self.task_types.map {|typ| {:type=>typ, :ids=>self.task_ids(typ)} }
+#  end
+#  
+  def self.tasks_hash(task_array)
+    self.task_types.map {|typ| {:type=>typ, :tasks=>self.tasks_with_type(task_array,typ)} }
+  end    
   
   # An array of all the types of task (like :pipeline ...), sorted according to the 
   # order they should be displayed. Currently this is defined in the 
@@ -45,4 +58,7 @@ class PersTask < ActiveRecord::Base
 #    [:pipeline, :orientation]
   end
   
+  def self.xx(id_str)
+    return [self.all]
+  end
 end
--- spec/models/pers_tasks_spec.rb
+++ spec/models/pers_tasks_spec.rb
@@ -2,22 +2,31 @@ require 'spec_helper'
 
 describe PersTask do
   before(:each) do
-    @t1 = PersTask.create(:task=>'t1', :pipeline=>true)
-    @t2 = PersTask.create(:task=>'t1', :pipeline=>true)
-    @x1 = PersTask.create(:task=>'t1', :orientation=>true)
-    @x2 = PersTask.create(:task=>'t1', :orientation=>true)
+    @t1 = PersTask.create(:task=>'pipeline_1', :pipeline=>true)
+    @t2 = PersTask.create(:task=>'pipeline_2', :pipeline=>true)
+    @x1 = PersTask.create(:task=>'orientation_1', :orientation=>true)
+    @x2 = PersTask.create(:task=>'orientation_2', :orientation=>true)
+    @all_tasks = [@t1, @t2, @x1, @x2]
   end
 
-  it 'returns list of task ids for given type' do
-    PersTask.task_ids(:pipeline).sort.should == [@t1.id, @t2.id].sort
-    PersTask.task_ids(:orientation).sort.should == [@x1.id, @x2.id].sort
+  it 'task_ids returns list of task ids for given type' do
+    result =  PersTask.tasks_with_type(@all_tasks, :pipeline)
+puts "**** result=#{result}"
+    result.should include @t1
+    result.should include @t2
+    result.should_not include @x1
+    result.should_not include @x2
   end    
   
-  it 'returns an ordered array of all tasks by type' do
-puts "**** PersTask.tasks_hash=#{PersTask.tasks_hash}"
-    PersTask.tasks_hash[0][:type].should == :pipeline
-    PersTask.tasks_hash[0][:ids].sort.should == [@t1.id, @t2.id].sort
+  it 'tasks_hash returns an ordered array of all tasks by type' do
+    result =  PersTask.tasks_hash(@all_tasks)
+# puts "**** result=#{result}"
+    result[0][:type].should == :pipeline
+    result[0][:tasks].should include @t1
+    result[0][:tasks].should include @t2
   end
       
+  it 'xx' do
+  end
 
 end
