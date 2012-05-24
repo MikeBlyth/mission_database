52d11caca49592673aac7e82b16caab0f49c1833
Mike Blyth <mike.blyth@sim.org>
2012-05-24 10:02:05 +0100

    Minor change to personnel tasks controller

--- app/controllers/pers_tasks_controller.rb
+++ app/controllers/pers_tasks_controller.rb
@@ -2,14 +2,8 @@ class PersTasksController < ApplicationController
   active_scaffold :pers_task do |config|
     list.sorting = {:task => 'ASC'}
     config.label = 'Personnel Tasks'
-    config.columns = [:task, :pipeline, :orientation, :start_of_term, :end_of_term, :end_of_service, :alert]
-    config.columns[:task].inplace_edit = true
-    config.columns[:pipeline].inplace_edit = true
-    config.columns[:orientation].inplace_edit = true
-    config.columns[:start_of_term].inplace_edit = true
-    config.columns[:end_of_term].inplace_edit = true
-    config.columns[:end_of_service].inplace_edit = true
-    config.columns[:alert].inplace_edit = true
+    config.columns = [:task] + Settings.pers_task_types + [:alert]
+    config.columns.each {|c| config.columns[c].inplace_edit = true }
     config.update.link = false  # Do not include a link to "Edit" on each line
 #    config.delete.link = false  # Do not include a link to "Delete" on each line
     config.show.link = false  # Do not include a link to "Show" on each line
