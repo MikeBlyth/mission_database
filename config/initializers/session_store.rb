ba1a8398f09c27e2b873908f29f50d469bfabe01
Mike Blyth <mike.blyth@sim.org>
2012-05-30 20:05:05 +0100

    Added timeout to cookies

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,8 +3,8 @@
 SIM::Application.config.session_store :cookie_store, 
     :key => '_SIM_session',
     :secure => Rails.env.production?, # Only send cookie over SSL when in production mode
-    :http_only => true # Don't allow Javascript to access the cookie (mitigates cookie-based XSS exploits)
-
+    :http_only => true, # Don't allow Javascript to access the cookie (mitigates cookie-based XSS exploits)
+    :expire_after => Rails.env.production? ? 3600 : 14400 # 1 hour/24 hours
 SIM::Application.config.secret_token = 'jklweoiuWMf;lk4760V302-098sqvngpo-5$YbN,uyQsxuwpp[n,s16v,sy/,/xs5-_'
 
 # Use the database for sessions instead of the cookie-based default,
