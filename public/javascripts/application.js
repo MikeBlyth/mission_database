c1349abcd20c1c129d2791dbcec8efe63bdd53f1
Mike Blyth <mike.blyth@sim.org>
2012-06-12 09:24:18 +0100

    Hiding SMS options when SMS not selected

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,24 +3,40 @@
 
 // Message page
 
-    $(function() { $('<p class="msg-info">At least <span id="msg-count">0</span> SMS messages will be sent</p>')
-      .insertAfter('#record_to_groups');
-      
+    $(function() { 
+        $("#record_body").charCount({
+            allowed: 150,	
+            warning: 20,
+            counterText: 'Characters left for one SMS message: '	
+            });      
+        $('<div><p class="msg-info for-sms">At least <span id="msg-count">0</span> SMS messages will be sent</p></div>')
+        .insertAfter('#record_to_groups');
+     toggle_sms_display();      
      });
 
 // SMS Selection
     $('#record_send_sms').live("change", function(){
         if ($(this).is(':checked')) {
           $('.counter').css('display','block')
-          $('#record_retries').css('display','inline')
-          $('label[for=record_retries]').css('display','inline')
+          $('.for-sms').css('display','inline')
           }
         else {
           $('.counter').css('display','none')
-          $('#record_retries').css('display','none')
-          $('label[for=record_retries]').css('display','none')
+          $('.for-sms').css('display','none')
         }  
       });
+
+    function toggle_sms_display() {
+        if ($('#record_send_sms').is(':checked')) {
+          $('.counter').css('display','block')
+          $('.for-sms').css('display','inline')
+          }
+        else {
+          $('.counter').css('display','none')
+          $('.for-sms').css('display','none')
+        } 
+    } ;
+
 
     $('#record_to_groups').live("change", function(){
  //       alert("Selected groups = " + $(this).val());
@@ -218,65 +234,6 @@ function set_spouse_choices(as_form) {
       }) 
     });
   };
-
-// ************************ CHARCOUNT ******************************
-// *********************************************************************
-// ** It might be worth putting this in a separate file since it's not used in most pages
-/*
-If you want to use this script, please keep the original author in this header!
-
-Purpose:	Script for applying maxlengths to textareas and monitoring their character lengths.
-Author: 	James O'Cull
-Date: 		08/14/08
-
-To use, simply apply a maxlenth value to a textarea.
-If you need it to prevent typing past a certain point, add lengthcut="true"
-
-Example:
-<textarea maxlength="1000" lengthcut="true"></textarea>
-
-If you add a new text area with javascript, simply call parseCharCounts() again find the new textarea(s) and label them!
-*/
-var LabelCounter = 0;
-
-function parseCharCounts()
-{
-	//Get Everything...
-	var elements = document.getElementsByTagName('textarea');
-	var element = null;
-	var maxlength = 9;
-	var newlabel = null;
-	
-	for(var i=0; i < elements.length; i++)
-	{
-		element = elements[i];
-		
-		if(element.getAttribute('maxlength') != null && element.getAttribute('limiterid') == null)
-		{
-			maxlength = element.getAttribute('maxlength');
-			
-			//Create new label
-			newlabel = document.createElement('label');
-			newlabel.id = 'limitlbl_' + LabelCounter;
-			newlabel.style.color = 'red';
-			newlabel.style.display = 'block'; //Make it block so it sits nicely.
-			newlabel.innerHTML = "Updating...";
-			
-			//Attach limiter to our textarea
-			element.setAttribute('limiterid', newlabel.id);
-			element.onkeyup = function(){ displayCharCounts(this); };
-			
-			//Append element
-			element.parentNode.insertBefore(newlabel, element);
-			
-			//Force the update now!
-			displayCharCounts(element);
-		}
-		
-		//Push up the number
-		LabelCounter++;
-	}
-}
 
 /*
  * 	Character Count Plugin - jQuery plugin
