c36c4822c7b36d8b9646973082defb5628f03eba
Mike Blyth <mike.blyth@sim.org>
2012-06-21 17:09:41 +0100

    in progress

--- app/gateways/clickatell_gateway.rb
+++ app/gateways/clickatell_gateway.rb
@@ -27,10 +27,21 @@ class ClickatellGateway < SmsGateway
     14 => {:our_status=>-1, :description=>"Maximum MT limit exceeded"}
     }
 
+  ExpiredSessionCode = '003'
+  AuthenticationFailedCode = '001'  
+
   def initialize
     @gateway_name = 'clickatell'
     @required_params = [:user_name, :password, :api_id]
     super
+  end
+
+  def credentials
+    "user=#{@user_name}&password=#{@password}&api_id=#{@api_id}"
+  end
+  
+  def base_uri
+    "http://api.clickatell.com/http/"
   end
 
   # Send a message (@body) to a phone (@numbers)
@@ -40,22 +51,66 @@ class ClickatellGateway < SmsGateway
     @numbers = numbers  # Update instance variables (matters only if they were included in this call)
     @body = body
     outgoing_numbers = numbers_to_string_list
-    clickatell_base_uri = "http://api.clickatell.com/http/sendmsg"
-    @uri = clickatell_base_uri + "?user=#{@user_name}&password=#{@password}&api_id=#{@api_id}"+
-            "&callback=2"+
+    @uri = base_uri + "sendmsg?&callback=2" +
             "&to=#{outgoing_numbers}&text=#{URI.escape(body)}"
     call_gateway
     super  # Note that it's called AFTER we make the connection to Clickatell, so it can include
            #   the results in the log.
   end
 
+  # Get the status for message with Clickatell ID = gw_msg_id
+  def query(gw_msg_id)
+    unless gw_msg_id.blank?
+      @uri = base_uri + "querymsg?&apimsgid=#{gw_msg_id}"
+      call_gateway
+      # If we got the status, then the ID will match the one we asked for and there will be Status: nnn
+      if @gateway_reply.body =~ /ID: (\w+) Status: (\w+)/ && ($1 == gw_msg_id)
+        return $2.to_i
+      else
+        return @gateway_reply.body
+      end
+    end
+  end
+            
+  # Clickatell uses sessions as an alternative to basic authentication. 
+  def get_session(try_again=true)
+    @uri = base_uri.sub('http://', 'https://') + "auth?" + credentials
+    reply = (HTTParty::get @uri).body
+    if reply =~ /OK: (\w+)/
+      @session = $1
+      return @session
+    else
+      @session = nil
+      return reply   # Which contains the error message
+    end
+  end
+    
+  def session_expired
+    @gateway_reply.body =~ Regexp.new("(Err: #{ExpiredSessionCode})|(Err: #{AuthenticationFailedCode})", 'i')
+  end
+  
+  def uri_with_session
+    @uri+"&session_id="+@session
+  end
+      
   # Connect to Clickatell via the URI.
   # This can be overridden for testing; mock method can simply provide the desired reply
   def call_gateway
-    @gateway_reply = HTTParty::get @uri #unless Rails.env.to_s == 'test'  # Careful with testing since this really sends messages!
+    if @uri =~ /password=/   # If we're using user_name and password, no need for session
+      @gateway_reply = HTTParty::get @uri
+    else
+      get_session if @session.nil?  # 
+      @gateway_reply = HTTParty::get uri_with_session 
+      # If we have used a session to call, and there is no valid session, then get one
+      if session_expired
+        get_session
+        @gateway_reply = HTTParty::get uri_with_session 
+      end
+    end
 #puts "**** CGtw#deliver @gateway_reply=#{@gateway_reply}"
   end
-    
+      
+ 
   def self.parse_status_params(params)
     gateway_msg_id = params[:apiMsgId]
     status = decode_status(params[:status])
--- spec/gateways/clickatell_gateway_spec.rb
+++ spec/gateways/clickatell_gateway_spec.rb
@@ -3,8 +3,22 @@ require 'sms_controller.rb'
 require 'sms_gateway.rb'
 
 describe ClickatellGateway do
+
+def gateway_session
+   @gateway.instance_variable_get(:@session)
+end
+
+def gateway_session_set(session)
+   @gateway.instance_variable_set(:@session, session)
+end
+
+def gateway_uri_set(uri)
+   @gateway.instance_variable_set(:@uri, uri)
+end
+
   before(:each) do
     @phones = ['+2347777777777']
+    @mock_reply = mock('gatewayReply', :body=>'')  # Remember to add stubs for body when something expected
   end
 
   describe 'when needed parameters are missing' do
@@ -36,12 +50,64 @@ describe ClickatellGateway do
     
   end # 'when needed parameters are present
 
+  describe 'Handles sessions:' do
+    before(:each) do
+      @gateway = ClickatellGateway.new
+      @session = 'eixktixyiis32kx00l'
+    end
+    
+    it 'gets a session' do
+      HTTParty.stub_chain(:get, :body).and_return("OK: #@session")
+      @gateway.get_session.should == @session
+      @gateway.instance_variable_get(:@session).should == @session
+    end
+
+    it 'returns error' do
+      response = mock('reply', :body=>'Err: 001, Authentication Failed')
+      HTTParty.stub(:get).and_return(response)
+      @gateway.get_session.should == response.body
+      gateway_session.should be_nil
+    end
+  end # Handles sessions
+  
+  describe 'Connects to gateway' do
+    before(:each) do
+      @gateway = ClickatellGateway.new
+      @httyparty = HTTParty
+      silence_warnings{HTTParty = mock('HTTParty')}
+     end
+    after(:each) do
+      silence_warnings{ HTTParty = @httyparty }  # Restore normal 
+    end
+    
+    it 'with basic authentication' do
+      test_uri = "http://etc/?password=and_so_on"
+#      @gateway.instance_variable_set(:@uri, test_uri)
+      gateway_uri_set(test_uri)
+      HTTParty.should_receive(:get).with(test_uri)
+      @gateway.call_gateway
+      gateway_session.should be_nil
+    end
+
+    it 'with a valid session' do
+      test_uri = "http://etc/do_something?blahblahblah"
+      gateway_uri_set(test_uri)
+      gateway_session_set('dummy')
+      @mock_reply.stub(:body).and_return('Fine')
+      HTTParty.should_receive(:get).with(test_uri+"&session_id=dummy").and_return(@mock_reply)
+      @gateway.call_gateway
+    end
+  end
+      
+    
+      
+
   describe 'Deliver method sends messages' do
       before(:each) do
         @gateway = ClickatellGateway.new
         @httyparty = HTTParty
         silence_warnings{HTTParty = mock('HTTParty')}
-        @mock_gateway_reply = "ID: ABCDEF"
+        @mock_gateway_reply = mock('reply', :body=>"ID: ABCDEF")
         HTTParty.stub(:get).and_return @mock_gateway_reply
         HTTParty.should_receive(:get)
       end
