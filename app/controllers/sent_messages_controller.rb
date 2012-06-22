6cbcab1bff80a026c2e29f6cb812439876706660
Mike Blyth <mike.blyth@sim.org>
2012-06-22 13:22:28 +0100

    in progress

--- app/controllers/sent_messages_controller.rb
+++ app/controllers/sent_messages_controller.rb
@@ -8,6 +8,7 @@ class SentMessagesController < ApplicationController
   load_and_authorize_resource
 
   def update_status_clickatell
+puts "Clickatell status call with params=#{params}"
     parsed = ClickatellGateway.parse_status_params(params)  # Let Gateway be responsible for understanding the params
     @sent_message = SentMessage.find_by_gateway_message_id parsed[:gateway_msg_id]  # The message whose status is being reported
     @sent_message.update_attributes(parsed[:updates]) if @sent_message
