73d7f379c3c31043a85487aa81172f6ab054f0cf
Mike Blyth <mike.blyth@sim.org>
2012-06-26 11:30:39 +0100

    in progress

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -53,6 +53,7 @@ load_and_authorize_resource
     fu_message = Message.create(params[:record].merge(:following_up => @id))  # a new message object to send the follow up
     fu_message.members = original_message.members_not_responding
     fu_message.deliver
+    flash[:notice] = 'Follow up message sent'
     redirect_to messages_path
   end
    
